eadc94c000305e9b52b3087e802d65fe2fca35e4
asmit990 <asmitpandeyw4354@gmail.com>
2024-12-24 22:35:10 +0530

    Added herfile.txt for her

---
+++ print/wow.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 25
+	name := "alice"
+    height := 6.342425243823840298503
+
+	fmt.Println("age :-", age, "name :-", name)
+
+
+	fmt.Printf("age is %d\n", age,"height is %0.2f\n",height)
+}
