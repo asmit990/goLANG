2e2d52d73bd005b64baf7c05cdd8100c7f5a2a48
asmit990 <asmitpandeyw4354@gmail.com>
2024-12-25 02:45:17 +0530

    array ke baare mei hai

---
+++ arrray/goooo.go
@@ -0,0 +1,21 @@
+package main 
+import "fmt"
+
+func main() {
+	fmt.Println("we are learning array in goLang ")
+
+	var name[5]string
+
+	name[0] = "prince"
+	name[2] = "akash"
+
+	fmt.Println("Name of Person is : ", name)
+
+	var numbers = [5]int{1,2,3,4,5}
+	fmt.Println("Number is : ", numbers)
+     
+	fmt.Println("length of Number array is : ", len(numbers))
+	fmt.Println("value of name at 2 index is ; ", name(2))
+   fmt.Println("name array is %q\n", name)
+	// go make the array default initialization with 
+}
