878b6f4c7961d72bb434a801168fe754698c6089
asmit990 <asmitpandeyw4354@gmail.com>
2024-12-25 15:07:10 +0530

    lolz

---
+++ switch-case-for-loop/switch.go
@@ -0,0 +1,17 @@
+package main 
+import "fmt"
+
+func main() {
+	day := 3
+
+	switch day {
+	case 1   :   // we can use the 23 4545 more things just to make the state same for all 
+	fmt.Println("sadfasfa")
+	case 2 :
+	fmt.Println("tuesday")
+	default : 
+	fmt.Println("Unknown day")
+	}
+}
+
+
