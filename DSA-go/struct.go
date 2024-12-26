e1b9875352d3c64b0d660e505921f6794a49c2fa
asmit990 <asmitpandeyw4354@gmail.com>
2024-12-26 06:22:37 +0530

    maps in go

---
+++ DSA-go/struct.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	FirstName string
+	LastName string
+	Age int
+
+}
+func main(){
+     var prince Person
+	 fmt.Println("Prince Person : ", prince)
+}
