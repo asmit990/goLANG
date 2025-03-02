4370e012cd8001ae11bc10d13c6326af03eaa665
asmit990 <asmitpandeyw4354@gmail.com>
2025-03-03 00:38:55 +0530

    shashank/anshul kyu dekh raha h mere commits

--- channels/goroutine/biderectional.go
+++ channels/goroutine/biderectional.go
@@ -5,16 +5,33 @@ import "fmt"
 
 func main() { 
 chanl1 := make(chan string)
-chanl2 := make(chan string)   
+chanl2 := make(chan string) 
 
 //if i use the make only send (chan ->string) unidirec
 // if i just c want to (-> chan string) 
 
 go sending(chanl1)
-valueFromchanel := <- chanl1
+valueFromChanel := <- chanl1
 fmt.Println("valueFromChanel", valueFromChanel)
-}
+go recieving(chanl2)
 
-func sending ( s chan string){
+chanl2 <- valueFromChanel
+
+
+chanl1 := make(chan string)
+go convert(chanl1)
+fmt.Println(<-chanl1)
+}
+func convert(s chan<- string){
+	s <- "some value"
+}
+func sending(s chan string){
 	s <- "go asmit"
+}
+func recieving(s chan string){
+	fmt.Println(<-s)
+
+}
+func multiplyWithChanel(ch chan int){
+
 }
